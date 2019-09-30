51786b30365b5fea7f354b33809f5ad4ade823e3
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-09-30 14:51:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def using_include(array, element)
-  using_include ["wow", "I", "am", "really", "learning", "arrays!"]
+  using_include = ["wow", "I", "am", "really", "learning", "arrays!"]
   using_include.include?("tardis")
 end
 
